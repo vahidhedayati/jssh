1062202ffee4f6f8656d516a8395e55bf2ef2900
vahid <vahid.hedayati@gmail.com>
2015-03-16 15:04:04 +0000

    bump

--- JsshGrailsPlugin.groovy
+++ JsshGrailsPlugin.groovy
@@ -3,14 +3,14 @@ import grails.plugin.jssh.J2sshEndpoint
 import grails.plugin.jssh.JsshConfig
 
 class JsshGrailsPlugin {
-    def version = "1.8"
+    def version = "1.8-SNAPSHOT"
 	def grailsVersion = "2.0 > *"
 	def title = "j2ssh SSH Plugin"
 	def author = "Vahid Hedayati"
 	def authorEmail = "badvad@gmail.com"
 	def description = 'Java J2SSH library combined with default websockets to provide your Grails web application with live SSH interaction functionality. Version 1 New Client/Server Websocket call: Make multiple connections and broadcast commands to a group of servers. You can either use default Websockets method which is a live interaction with your back-end ssh connection(s) or use Ajax polling which only supports 1 concurrent connection at any one time.'
 	def documentation = "http://grails.org/plugin/jssh"
-	def license = "APACHE"
+	def license = "Apache-2.0"
 	def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/jssh/issues']
 	def scm = [url: 'https://github.com/vahidhedayati/jssh']
 
@@ -18,9 +18,7 @@ class JsshGrailsPlugin {
 		jsshConfig(JsshConfig) {
 			grailsApplication = ref('grailsApplication')
 		} 
-		connectSsh(ConnectSsh) {
-			
-		}
+		connectSsh(ConnectSsh)
 	}
 	
 	def doWithWebDescriptor = { xml ->
